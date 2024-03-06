f75c6afbf66841bab4e9ad96a83ded3578b45661
Ryu7ken <baquiur.rahman@outlook.com>
2024-03-06 19:18:43 +0530

    changed css

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,8 +6,9 @@ const Login = () => {
     return (
         <div id="login-page" className="w-full h-screen flex justify-center items-center">
 
-            <div id="login-card" className="w-[420px] h-56 md:w-[640px] md:h-64 bg-white bg-opacity-25 text-center pt-9 pb-16 rounded-xl">
-                <h2 className="text-3xl md:text-4xl font-bold my-3">Welcome to Interacto 💬</h2>
+            <div id="login-card" className="w-80 h-60 sm:w-[420px] md:w-[500px] md:h-64 bg-white bg-opacity-25 text-center pt-7 pb-16 rounded-xl">
+                <h2 className="text-3xl md:text-4xl font-bold my-2">Hey, Welcome to</h2>
+                <h2 className="text-3xl md:text-4xl font-bold">Interacto 💬</h2>
 
                 <div className="login-button cursor-pointer mt-8 md:mt-12 inline-block"
                 onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
